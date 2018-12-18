52222e107fb4cb499010faf5148e6304dce46b47
fey <feycot@gmail.com>
2018-12-18 09:10:48 +0500

    refactor Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,23 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function welcome()
 {
     line('Welcome to the Brain Game!');
+}
+function askName()
+{
     $name = prompt('May I have your name?');
-    line('Hello, %s!', $name);
+    return $name;
+}
+function printHello($name)
+{
+    line(PHP_EOL . 'Hello, %s! '. PHP_EOL, $name) ;
+}
+
+function run($game)
+{
+    welcome();
+    $name = askName();
+    printHello();
 }
