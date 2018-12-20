113c12baa80958456cf54093b42bb99feb19a62f
fey cot <feycot@gmail.com>
2018-12-20 08:21:24 +0000

    refactoring Progression.php

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -10,43 +10,32 @@ const PROGRESSION_SIZE = 10;
 function game()
 {
     $game = function () {
-        $progression = generateProgression();
+        $firstElement = rand(1, 100);
+        $step = rand(1, 5);
+        $progression = generateProgression($firstElement, $step, PROGRESSION_SIZE);
         $indexOfHidden = array_rand($progression);
-        $answer = getGameAnswer($progression, $indexOfHidden);
-        $question = generateQuestion($progression, $indexOfHidden);
+
+        $answer ="{$progression[$indexOfHidden]}";
+        $question = function () use ($progression, $indexOfHidden) {
+            $progression[$indexOfHidden] = '..';
+            return implode(" ", $progression);
+        };
 
         return [
-            'question' => $question,
+            'question' => $question(),
             'answer'   => $answer,
         ];
     };
     play(DESCRIPTION, $game);
 }
 
-function generateProgression()
+function generateProgression($firstElement, $step, $size)
 {
-    $firstElement = rand(1, 100);
-    $step = rand(1, 5);
-    $iter = function ($acc) use ($step, &$iter) {
-        $currentSize = count($acc);
-        if ($currentSize < PROGRESSION_SIZE) {
-            $acc[] = $acc[$currentSize - 1] + $step;
-            return $iter($acc);
-        }
+    $progression = [];
 
-        return $acc;
-    };
+    for($i = 0; $i < $size; $i += 1) {
+        $progression[] = $firstElement + $step * $i;
+    }
 
-    return $iter([$firstElement]);
-}
-
-function getGameAnswer($progression, $indexOfHidden): string
-{
-    return $progression[$indexOfHidden];
-}
-
-function generateQuestion($progression, $hiddenIndex):string
-{
-    $progression[$hiddenIndex] = '..';
-    return implode(" ", $progression);
+    return $progression;
 }
