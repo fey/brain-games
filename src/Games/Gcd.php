aa9f041a4b9b5fa781e8975ab6c1e49676f4925b
fey cot <feycot@gmail.com>
2018-12-20 07:56:28 +0000

    remove getQuestion()

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -11,7 +11,7 @@ function game()
     $game = function () {
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
-        $question = getQuestion($num1, $num2);
+        $question = "{$num1} {$num2}";
         $answer = gcd($num1, $num2);
         return [
             'question' => $question,
@@ -28,9 +28,4 @@ function gcd($num1, $num2):string
     $modulo = ($min === 0) ? 0 : ($max % $min);
 
     return ($modulo === 0) ? $min : gcd($min, $modulo);
-}
-
-function getQuestion($num1, $num2)
-{
-    return "{$num1} {$num2}";
 }
