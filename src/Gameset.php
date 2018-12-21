f61fbc18fe0d655a523f093785acda9fff6e75f6
fey <feycot@gmail.com>
2018-12-21 21:45:10 +0500

    add Gameset.php

---
+++ src/Gameset.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Games\Calc\game' => 'brain-calc',
+    'Games\IsEven\game' => 'brain-even',
+    'Games\IsPrime\game' => 'brain-prime',
+    'Games\Progression\game' => 'brain-progression',
+    'Games\Gcd\game' => 'brain-gcd'
+];
