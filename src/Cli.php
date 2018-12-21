95447d26af69dc16740dbcbf9110fa23ce7919b7
fey cot <feycot@gmail.com>
2018-12-21 08:03:10 +0000

    add games menu

--- src/Cli.php
+++ src/Cli.php
@@ -9,12 +9,13 @@ const ROUNDS = 3;
 
 function run($description = "", $game = null)
 {
-    line('Welcome to the Brain Game!');
-    line($description . PHP_EOL);
-    $playerName = prompt('May I have your name?');
-    line(PHP_EOL . 'Hello, %s! ' . PHP_EOL, $playerName);
+    line('Welcome to the Brain Games!');
 
+    $games = require_once(__DIR__ . '/menu.php');
     if ($game) {
+        line($description . PHP_EOL);
+        $playerName = prompt('May I have your name?');
+        line(PHP_EOL . 'Hello, %s! ' . PHP_EOL, $playerName);
         for ($i = 1; $i <= ROUNDS; $i += 1) {
             ['question' => $question, 'answer' => $gameAnswer] = $game();
             line('Question: %s', $question);
@@ -25,10 +26,17 @@ function run($description = "", $game = null)
             } else {
                 line("'%s' is wrong answer ;(. Correct answer was '%s'." . PHP_EOL, $playerAnswer, $gameAnswer);
                 line("Let's try again, %s!", $playerName);
-                return;
+                return run();
             }
         }
         line("Congratulations, {$playerName}!");
         return;
+    } else {
+
+        $game = \cli\menu($games, $default = false, $title = 'Choose a game number');
+        if(function_exists($game)) {
+            return $game();
+        }
+        line("Wrong game number!");
     }
 }
---
+++ src/menu.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Games\Calc\game'        => 'brain-calc',
+    'Games\IsEven\game'      => 'brain-even',
+    'Games\Gcd\game'         => 'brain-gcd',
+    'Games\Progression\game' => 'brain-progression'
+];
