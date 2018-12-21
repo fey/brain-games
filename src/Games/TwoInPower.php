a9ba812d0674e153c15b747099b82570e056e042
fey <feycot@gmail.com>
2018-12-21 07:57:08 +0500

    bonus game

---
+++ src/Games/TwoInPower.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Games\TwoInPower;
+
+use function BrainGames\Cli\run;
+
+const DESCRIPTION = 'What are exponent of two?';
+
+
+function game()
+{
+    $game = function () {
+        $pow      = rand(1, 10);
+        $question = "2 ^ {$pow}";
+        $answer   = strval(pow(2, $pow));
+        return [
+            'question' => $question,
+            'answer'   => $answer,
+        ];
+    };
+    run(DESCRIPTION, $game);
+}
