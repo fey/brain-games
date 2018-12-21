d485ff6a37cd7286c781a0fb03d8c5d3c80e8e98
fey <feycot@gmail.com>
2018-12-21 21:59:39 +0500

    lint

--- src/Gameset.php
+++ src/Gameset.php
@@ -1,8 +1,9 @@
 <?php
 return [
-    'Games\Calc\game' => 'brain-calc',
-    'Games\IsEven\game' => 'brain-even',
-    'Games\IsPrime\game' => 'brain-prime',
+    'Games\Calc\game'        => 'brain-calc',
+    'Games\IsEven\game'      => 'brain-even',
+    'Games\IsPrime\game'     => 'brain-prime',
     'Games\Progression\game' => 'brain-progression',
-    'Games\Gcd\game' => 'brain-gcd'
+    'Games\Gcd\game'         => 'brain-gcd',
+    'Games\TwoInPower\game'  => 'brain-power'
 ];
