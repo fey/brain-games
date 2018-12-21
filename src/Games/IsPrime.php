b7cd96486466670b7a729221828082801d4a4478
fey cot <feycot@gmail.com>
2018-12-21 11:21:40 +0000

    refactoring isPrime

--- src/Games/IsPrime.php
+++ src/Games/IsPrime.php
@@ -25,16 +25,13 @@ function isPrime(int $number): bool
     if ($number < 2) {
         return false;
     }
-    $iter = function ($delim) use ($number, &$iter) {
-        if ($delim ** 2 <= $number) {
-            if ($number % $delim === 0) {
-                return false;
-            }
-            return $iter($delim + 1);
+    for($delim = 2; $delim ** 2 <= $number; $delim += 1) {
+        if ($number % $delim === 0) {
+            return false;
         }
-        return true;
-    };
-    return $iter(2);
+    }
+    return true;
+
 }
 
 function getGameAnswer(int $number): string
