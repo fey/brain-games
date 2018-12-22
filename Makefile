2a09a89b04c4134140b33986187a83425644df78
fey cot <feycot@gmail.com>
2018-12-22 10:49:46 +0000

    add Balance.php make task fix lint

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ install:
 	composer install
 lint:
 	composer run-script phpcs -- --standard=PSR12 src bin
+fix-lint:
+	composer run-script phpcbf -- --standard=PSR12 src bin
---
+++ src/Games/Balance.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Games\Balance;
+
+use function BrainGames\Cli\run;
+
+const DESCRIPTION = 'Balance the given number.';
+
+function game()
+{
+    $game = function () {
+        $question;
+        $answer;
+        return [
+            'question' => $question,
+            'answer'   => $answer,
+        ];
+    };
+    run(DESCRIPTION, $game);
+}
